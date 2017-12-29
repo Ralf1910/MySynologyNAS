364855f49f4c309dfc0c3e24205b72e21e5a3150
Ralf1910 <home@ralf-ehren.de>
2017-12-29 12:36:28 +0100

    Update module.php

--- SynologyNAS/module.php
+++ SynologyNAS/module.php
@@ -32,7 +32,7 @@ class SynologyNAS extends IPSModule {
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("SystemTemperature", "System - Temperatur", "Temperatur", 10), true);
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("SystemPowerStatus", "System - Power Status", "", 10), true);
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("SystemSystemFanStatus", "System - System Fan Status", "", 10), true);
-		AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("SystemCPUStatus", "System - CPU Fan Status", "", 10), true);
+		AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("SystemCPUFanStatus", "System - CPU Fan Status", "", 10), true);
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableString("SystemModelName", "System - Model Name", "", 10), true);
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableString("SystemSerialNumber", "System - Serial Number", "", 10), true);
 		AC_SetLoggingStatus($archiv, $this->RegisterVariableString("SystemVersion", "System - Version", "", 10), true);
@@ -46,8 +46,32 @@ class SynologyNAS extends IPSModule {
 		//Instanz ist aktiv
 		$this->SetStatus(102);
 	}
+	
+	//
+	private function getSnmpData($oid) {
+		return exec("c:\ip-symcon\snmpget.exe -c:home -v:2c -q -r:192.168.251.51 -o:".$oid);
+	}
 	// Aktualisierung der Variablen
 	public function Update() {
+		$SynologyData['System']['systemStatus'] = getSnmpData("1.3.6.1.4.1.6574.1.1.0");
+		$SynologyData['System']['temperature'] = getSnmpData("1.3.6.1.4.1.6574.1.2.0");
+		$SynologyData['System']['powerStatus'] = getSnmpData("1.3.6.1.4.1.6574.1.3.0");
+		$SynologyData['System']['systemFanStatus'] = getSnmpData("1.3.6.1.4.1.6574.1.4.1.0");
+		$SynologyData['System']['cpuFanStatus'] = getSnmpData("1.3.6.1.4.1.6574.1.4.2.0");
+		$SynologyData['System']['modelName'] = getSnmpData("1.3.6.1.4.1.6574.1.5.1.0");
+		$SynologyData['System']['serialNumber'] = getSnmpData("1.3.6.1.4.1.6574.1.5.2.0");
+		$SynologyData['System']['version'] = getSnmpData("1.3.6.1.4.1.6574.1.5.3.0");
+		$SynologyData['System']['upgradeAvailable'] = getSnmpData("1.3.6.1.4.1.6574.1.5.4.0");
+		
+		SetValue($this->GetIDforIdent("SystemStatus"), $SynologyData['System']['systemStatus']);
+		SetValue($this->GetIDforIdent("SystemTemperature"), $SynologyData['System']['temperature']);
+		SetValue($this->GetIDforIdent("SystemPowerStatus"), $SynologyData['System']['powerStatus']);
+		SetValue($this->GetIDforIdent("SystemFanStatus"), $SynologyData['System']['systemFanStatus']);
+		SetValue($this->GetIDforIdent("SystemCPUFANStatus"), $SynologyData['System']['cpuFanStatus']);
+		SetValue($this->GetIDforIdent("SystemModelName"), $SynologyData['System']['modelName']);
+		SetValue($this->GetIDforIdent("SystemSerialNumber"), $SynologyData['System']['serialNumber']);
+		SetValue($this->GetIDforIdent("SystemVersion"), $SynologyData['System']['version']);
+		SetValue($this->GetIDforIdent("SystemUpgradeAvailable"), $SynologyData['System']['upgradeAvailable']);
 		// $power = Sys_Ping($this->ReadPropertyString("IPAdresse"), 1000 );
 		// SetValue($this->GetIDForIdent("Power"), $power);
 	}
