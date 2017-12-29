838dd43e363c482632a4964afb7477d510ee8740
Ralf1910 <home@ralf-ehren.de>
2017-12-29 12:10:14 +0100

    Update module.php

--- SynologyNAS/module.php
+++ SynologyNAS/module.php
@@ -33,3 +33,21 @@ class SynologyNAS extends IPSModule {
 		SetValue($this->GetIDForIdent("Power"), $power);
 	}
 }
+
+
+if ($this->ReadPropertyInteger("LeistungWR1")>0) {
+				if (strlen($this->ReadPropertyString("WR1Pac"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1LeistungAC", "WR1 Leistung AC", "Elektrizitaet.Leistung", 100), true);
+				if (strlen($this->ReadPropertyString("WR1DaySum"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1ErzeugteEnergie", "WR1 erzeugte Energie", "Elektrizitaet.Verbrauch", 110), true);
+				if (strlen($this->ReadPropertyString("WR1DaySum"))>0)	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1RollierenderJahresertrag", "WR1 rollierender Jahresertrag", "Elektrizitaet.Verbrauch", 120), true);
+				if (strlen($this->ReadPropertyString("WR1Pdc1"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1LeistungDC1", "WR1 Leistung DC1", "Elektrizitaet.Leistung", 130), true);
+				if (strlen($this->ReadPropertyString("WR1Pdc2"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1LeistungDC2", "WR1 Leistung DC2", "Elektrizitaet.Leistung", 130), true);
+				if (strlen($this->ReadPropertyString("WR1Pdc3"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1LeistungDC3", "WR1 Leistung DC3", "Elektrizitaet.Leistung", 130), true);
+				if (strlen($this->ReadPropertyString("WR1Udc1"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1SpannungDC1", "WR1 Spannung DC1", "Elektrizitaet.Spannung_DC", 140), true);
+				if (strlen($this->ReadPropertyString("WR1Udc2"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1SpannungDC2", "WR1 Spannung DC2", "Elektrizitaet.Spannung_DC", 140), true);
+				if (strlen($this->ReadPropertyString("WR1Udc3"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1SpannungDC3", "WR1 Spannung DC3", "Elektrizitaet.Spannung_DC", 140), true);
+				if (strlen($this->ReadPropertyString("WR1Temp"))>0) 	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1Temperatur", "WR1 Temperatur", "~Temperature", 150), true);
+				if (strlen($this->ReadPropertyString("WR1Uac"))>0)  	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1Wirkungsgrad", "WR1 Wirkungsgrad", "Elektrizitaet.Wirkungsgrad", 160), true);
+				if (strlen($this->ReadPropertyString("WR1Uac"))>0)  	AC_SetLoggingStatus($archiv, $this->RegisterVariableFloat("WR1SpannungAC", "WR1 Spannung AC", "Elektrizitaet.Spannung_230V", 170), true);
+				if (strlen($this->ReadPropertyString("WR1Status"))>0)  	AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("WR1Status", "WR1 Status", "", 180), true);
+				if (strlen($this->ReadPropertyString("WR1Error"))>0)  	AC_SetLoggingStatus($archiv, $this->RegisterVariableInteger("WR1Fehlermeldung", "WR1 Fehlermeldung", "", 190), true);
+			}
